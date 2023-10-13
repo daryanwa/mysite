3bb23f4fb8b4c5daa5389ebda653867d601619ce
Rostislav <asawww100@gmail.com>
2023-10-14 00:28:36 +0300

    last update

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,26 +7,29 @@ function Main() {
 
 
 
+
+
   return (
     <div>
-        <section className='about-section-container'>
+        <div id='section1' className='about-section-container'>
+    
             <p className='studio-container'>St<span className='u-container'>u</span>dio</p>
             <div className='background-image'></div>
             <p className='benoy-container'>BENOY</p> 
             <p className='international'>International Architecture, Masterplanning, <br/> Interior and Graphic Design Studio</p>
-        </section>
-        <section className='blur-section'>
+        </div>
+        <div className='blur-section'>
             <p>DESIGN, <br/>
                  LANDING <br/>
                   LANDSCAPE</p>
-        </section>
-        <section className='blur-section-right'>
+        </div>
+        <div className='blur-section-right'>
             <p>
                 ARC <br/>
                 BUIL
             </p>
-        </section>
-        <section className='text-container'>
+        </div>
+        <div  className='text-container'>
             <ul>
                 <li>About</li>
             </ul>
@@ -41,16 +44,29 @@ With a focus on agile, flexible, and experiential design, we tailor our thinking
 
 Unconstrained by sectoral parameters, building formats, head office agendas or egos, we focus on agile, flexible, and experiential design. Design that is tailored to the needs of the people who live in, work in, and use the destinations we create.
 </p>
+       
             </div>
-        </section>
+        </div>
         <section className='img-container'>
             <div className='img-three'></div>
             <div className='img-two'></div>
             <p className='text-left-img'>Transforming the world we live in</p>
         </section>
-        <section className='expertise-container'>
+        <div className='img-container-expertise'>
+                <div className='img-container-left-one'></div>
+                <div className='img-container-right-two'></div>
+            </div>
+        <section id='section2' className='expertise-container'>
+            <p className='expertise-name'>EXPERTISE</p>
+      
 
+            <ul className='expertise-ul'>
+                <li>Urban Design <span className='dujki'>(32)</span></li>
+                <li>Architecture and Buildings <span className='dujki'>(73)</span></li>
+                <li>Interior Design <span className='dujki'>(61)</span></li>
+            </ul>
         </section>
+      
     </div>
   )
 }
